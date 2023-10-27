ff712b477eb083ce919d66e5a16e023605286a22
Minjik06 <minjik525@deltalearns.ca>
2023-10-27 13:30:22 -0700

    sdf

--- task01.py
+++ task01.py
@@ -62,3 +62,66 @@ Enter in the scores for 10 students for Assignment 2:
 """
 
 import json
+class  assignment():
+    assign={}
+    ID=[]
+    ID1=0
+    AssignmentValue=[]
+    AssignmentName=[]
+    def createAssignment(self):
+        #print("please Enter name and value with _ _")
+        a=str(input("Enter the assignment name: "))
+        b=int(input("Enter in Assignment Value: "))
+        print(f"Your assignment has been assigned ID {self.ID1}")
+        self.AssignmentValue.append(b)
+        self.AssignmentName.append(a)
+        self.ID.append(self.ID1)
+        self.ID1+=1
+
+        dict={}                   #Assign={Assignment1:[]}
+        for i in range(b):
+            dict[i+1]=0
+        self.assign[a]=dict
+        print(self.assign)
+
+        print()
+        self.__init__()
+
+
+            
+
+    def assignmentScore(self):
+        print(f"Current Assignment ID you can search : {self.ID}")
+        a=int(input("Enter in the assignment ID: "))  #0
+
+        print(f"Enter in the scores for {self.AssignmentValue[a]} students for {self.AssignmentName[a]}: ")
+        for i in range(self.AssignmentValue[a]):
+            b=int(input(f"{i+1}: "))
+            self.assign[self.AssignmentName[a]][i+1]=b
+
+        print(self.assign)
+        print()
+        self.__init__()
+
+    def writeData(self):
+        file=json.dumps(self.assign)
+        print(file)
+        print(type(file))
+
+
+    def __init__(self):
+        a=int(input("1. Create an Assignment\n2. Enter in Assignment Scores\n3. Write your data to file\nEnter in your choice: "))
+        if a==1:
+            print()
+            self.createAssignment()
+        elif a==2:
+            print()
+            self.assignmentScore()
+        else:
+            print()
+            self.writeData()
+            
+
+
+
+a=assignment()
